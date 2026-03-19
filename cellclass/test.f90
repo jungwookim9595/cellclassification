0ca892f4a0090547348e8a451f4cb6d53d4ab3f1
jungwookim9595 <yasandy@yonsei.ac.kr>
2026-03-19 09:01:32 +0900

    .

--- cellclass/test.f90
+++
@@ -1,5 +0,0 @@
-program test
-  character(len=100) :: fileout
-  write(fileout, '(1A37,3(A1,I1),1A4)') 'output/result/G/out_heavi','_', 0, '_', 0, '_', 0, '.dat'
-  print *, ">>", trim(fileout), "<<"
-end program test
